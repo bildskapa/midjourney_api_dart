ec3d384dfc12ad18385825a3599b7388498beca8
Michael Lazebny <michael@lazebny.io>
2024-10-02 18:41:19 +0200

    refactored impl class

--- lib/src/client/midjourney_client.dart
+++ lib/src/client/midjourney_client.dart
@@ -6,7 +6,13 @@ import 'package:midjourney_api_dart/src/const/config.dart';
 import 'package:midjourney_api_dart/src/model/function.dart';
 import 'package:midjourney_api_dart/src/model/midjourney_response.dart';
 
+/// Implementation of the MidjourneyClient interface.
+/// This class provides methods to interact with the Midjourney API.
 final class MidjourneyClientImpl implements MidjourneyClient {
+  /// Creates a new instance of [MidjourneyClientImpl].
+  ///
+  /// [config] is required and contains the configuration for the Midjourney API.
+  /// [httpClient] is optional and can be provided for custom HTTP handling.
   MidjourneyClientImpl({
     required MidjourneyConfig config,
     http.Client? httpClient,
@@ -16,6 +22,12 @@ final class MidjourneyClientImpl implements MidjourneyClient {
   final MidjourneyConfig _config;
   final http.Client _httpClient;
 
+  /// Submits an 'imagine' job to the Midjourney API.
+  ///
+  /// [prompt] is the text prompt for image generation.
+  /// [channelId] is the ID of the channel where the job will be submitted.
+  /// [function] specifies the Midjourney function to use.
+  /// [roomId] is optional and can be used to specify a room.
   @override
   Future<MidjourneyJobResponse> imagine({
     required String prompt,
@@ -38,26 +50,17 @@ final class MidjourneyClientImpl implements MidjourneyClient {
       },
     );
 
-    if (response
-        case {
-          'success': List<Object?> success,
-          'failure': List<Object?> failure,
-        }) {
-      return MidjourneyJobResponse(
-        success: success
-            .whereType<Map<String, Object?>>()
-            .map(MidjourneyJobSuccess.fromJson)
-            .toList(growable: false),
-        failure: failure
-            .whereType<Map<String, Object?>>()
-            .map(MidjourneyJobFailure.fromJson)
-            .toList(growable: false),
-      );
-    }
-
-    throw Exception('Unexpected JSON structure: $response');
+    return _parseJobResponse(response);
   }
 
+  /// Submits an 'upscale' job to the Midjourney API.
+  ///
+  /// [id] is the ID of the image to upscale.
+  /// [channelId] is the ID of the channel where the job will be submitted.
+  /// [function] specifies the Midjourney function to use.
+  /// [type] is the type of upscale operation.
+  /// [index] is the index of the image to upscale.
+  /// [roomId] is optional and can be used to specify a room.
   @override
   Future<MidjourneyJobResponse> upscale({
     required String id,
@@ -84,26 +87,12 @@ final class MidjourneyClientImpl implements MidjourneyClient {
       },
     );
 
-    if (response
-        case {
-          'success': List<Object?> success,
-          'failure': List<Object?> failure,
-        }) {
-      return MidjourneyJobResponse(
-        success: success
-            .whereType<Map<String, Object?>>()
-            .map(MidjourneyJobSuccess.fromJson)
-            .toList(growable: false),
-        failure: failure
-            .whereType<Map<String, Object?>>()
-            .map(MidjourneyJobFailure.fromJson)
-            .toList(growable: false),
-      );
-    }
-
-    throw Exception('Unexpected JSON structure: $response');
+    return _parseJobResponse(response);
   }
 
+  /// Submits jobs to the Midjourney API.
+  ///
+  /// This is a private method used by both [imagine] and [upscale].
   Future<Map<String, Object?>> _submitJobs({
     required String type,
     required String channelId,
@@ -130,7 +119,7 @@ final class MidjourneyClientImpl implements MidjourneyClient {
     );
 
     if (response.statusCode != 200) {
-      throw Exception('Failed to submit jobs: ${response.body}');
+      throw Exception('Failed to submit jobs: ${response.statusCode} ${response.body}');
     }
 
     final json = jsonDecode(response.body);
@@ -140,5 +129,25 @@ final class MidjourneyClientImpl implements MidjourneyClient {
     }
 
     return json;
+  }
+
+  /// Parses the job response from the API.
+  ///
+  /// This method is used to convert the API response into a [MidjourneyJobResponse] object.
+  MidjourneyJobResponse _parseJobResponse(Map<String, Object?> response) {
+    if (response case {'success': List<Object?> success, 'failure': List<Object?> failure}) {
+      return MidjourneyJobResponse(
+        success: success
+            .whereType<Map<String, Object?>>()
+            .map(MidjourneyJobSuccess.fromJson)
+            .toList(growable: false),
+        failure: failure
+            .whereType<Map<String, Object?>>()
+            .map(MidjourneyJobFailure.fromJson)
+            .toList(growable: false),
+      );
+    }
+
+    throw Exception('Unexpected JSON structure: $response');
   }
 }
