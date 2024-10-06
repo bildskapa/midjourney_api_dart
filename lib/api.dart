11f0decd4a1a406e6bb5edf013b7fe8b942b167c
Michael Lazebny <michael@lazebny.io>
2024-10-06 21:19:47 +0200

    export Midjourney WS Event

--- lib/api.dart
+++ lib/api.dart
@@ -4,4 +4,5 @@ export 'src/client/midjourney_ws_client.dart';
 export 'src/const/config.dart';
 export 'src/model/function.dart';
 export 'src/model/midjourney_response.dart';
+export 'src/model/midjourney_ws_event.dart';
 export 'src/utils/logger.dart';
