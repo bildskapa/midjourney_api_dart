2288dba1ef5ce5711f83ba7da6eabee51f5e132a
Michael Lazebny <michael@lazebny.io>
2024-10-14 13:33:13 +0200

    export thomas job

--- lib/api.dart
+++ lib/api.dart
@@ -4,4 +4,5 @@ export 'src/client/midjourney_ws_client.dart';
 export 'src/model/function.dart';
 export 'src/model/midjourney_response.dart';
 export 'src/model/midjourney_ws_event.dart';
+export 'src/model/thomas_job.dart';
 export 'src/utils/logger.dart';
